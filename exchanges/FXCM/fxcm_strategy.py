c0fb72da91f4426249060bf1cfda751fec4af994
Janakiraman Ganesan <vg2jana@gmail.com>
2020-03-05 19:06:03 +0530

    Revise take profit and socket reconnect.

--- exchanges/FXCM/fxcm_strategy.py
+++ exchanges/FXCM/fxcm_strategy.py
@@ -2,83 +2,117 @@ import random
 import math
 import json
 import fxcmpy
+import os
+import sys
+
+
+def connect():
+    global con
+    print("Reconnecting to FXCM...")
+    con = fxcmpy.fxcmpy(access_token=data['global']['token'], log_level='error', server='demo')
+    return con
 
 
 class Symbol:
-    def __init__(self, symbol, connection, config):
+    def __init__(self, symbol, config):
         self.symbol = symbol
-        self.con = connection
         self.is_subscribed = False
         self.positions = None
         self.config = config
 
     def subscribe(self):
-        self.con.subscribe_market_data(self.symbol)
+        con.subscribe_market_data(self.symbol)
+
+    def close_positions(self, pos, ohlc):
+        close = False
+        tp = self.config['takeProfit']
+        if len(self.positions) == 1:
+            tp = tp / 2
+
+        if pos['isBuy'] is True:
+            if ohlc['Bid'] > pos['open'] + tp:
+                close = True
+        else:
+            if ohlc['Ask'] < pos['open'] - tp:
+                close = True
+
+        return close
 
     def run(self):
         if self.positions is None:
+            if os.path.exists('STOP'):
+                return
             isBuy = random.choice([True, False])
             amount = self.config['amount']
             if isBuy is True:
-                self.con.create_market_buy_order(self.symbol, amount)
+                con.create_market_buy_order(self.symbol, amount)
             else:
-                self.con.create_market_sell_order(self.symbol, amount)
+                con.create_market_sell_order(self.symbol, amount)
         else:
-            pos = self.positions[self.positions['amounK'] == self.positions['amountK'].max()]
+            pos = self.positions[self.positions['amountK'] == self.positions['amountK'].max()]
             pos = pos.to_dict('records')[0]
-            amount = math.ceil(pos['amountK'] * self.config['ratio'])
-            ohlc = self.con.get_last_price(self.symbol).to_dict()
+            # amount = math.ceil(pos['amountK'] * self.config['ratio'])
+            amount = pos['amountK'] + self.config['amount']
+            ohlc = con.get_last_price(self.symbol).to_dict()
 
-            close = False
             if pos['isBuy'] is True:
-                if ohlc['Bid'] > pos['open'] + self.config['takeProfit']:
-                    close = True
                 isBuy = False
                 price = pos['open'] - self.config['swing']
             else:
-                if ohlc['Ask'] < pos['open'] - self.config['takeProfit']:
-                    close = True
                 isBuy = True
                 price = pos['open'] + self.config['swing']
 
-            if close is True:
-                self.con.close_all_for_symbol(self.symbol)
+            if self.close_positions(pos, ohlc) is True:
+                con.close_all_for_symbol(self.symbol)
             elif isBuy is True and ohlc['Bid'] > price:
-                self.con.create_market_buy_order(self.symbol, amount)
+                con.create_market_buy_order(self.symbol, amount)
             elif isBuy is False and ohlc['Ask'] < price:
-                self.con.create_market_sell_order(self.symbol, amount)
+                con.create_market_sell_order(self.symbol, amount)
 
 
 class SwingTrading:
-    def __init__(self, symbols, con):
+    def __init__(self, symbols):
         self.symbols = symbols
         self.open_positions = None
-        self.con = con
 
     def before_run(self):
-        df = self.con.get_open_positions()
+        while True:
+            try:
+                df = con.get_open_positions()
+            except Exception as e:
+                print(e)
+                try:
+                    connect()
+                except Exception as e:
+                    print(e)
+            else:
+                break
         if df.empty is True:
+            if os.path.exists('STOP'):
+                sys.exit(0)
             return
         for symbol in self.symbols:
-            if symbol in df.columns:
+            if symbol.symbol in df.currency.values:
                 symbol.positions = df.loc[df['currency'] == symbol.symbol]
 
     def run(self):
         for symbol in self.symbols:
-            if self.con.is_subscribed(symbol.symbol) is False:
+            if con.is_subscribed(symbol.symbol) is False:
                 symbol.subscribe()
             symbol.run()
 
 
-data = json.load('config.json')
-con = fxcmpy.fxcmpy(access_token=data['global']['token'], log_level='error', server='demo')
+con = None
+with open('config.json', 'r') as f:
+    data = json.load(f)
+connect()
 
 symbols = []
 for s, c in data["symbols"].items():
-    symbol = Symbol(s, con ,c)
+    symbol = Symbol(s, c)
     symbols.append(symbol)
 
-swing = SwingTrading(symbols, con)
+swing = SwingTrading(symbols)
 
 while True:
     swing.before_run()
