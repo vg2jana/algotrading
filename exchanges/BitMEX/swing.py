8bfe45b75666b41b7eeff515f234a37188a3a0ef
Janakiraman Ganesan <vg2jana@gmail.com>
2020-03-19 13:22:19 +0530

    BitMEX stop order fix

--- exchanges/BitMEX/swing.py
+++ exchanges/BitMEX/swing.py
@@ -112,15 +112,15 @@ class User:
         next_order = self.next_stop_order
         if next_order is None and opposite_open is True and curr_qty < opp_qty and opposite.next_stop_order is None \
                 and opp_tp_order is not None:
-            # if opp_tp_order['ordStatus'] == 'Filled':
-            #     return
-            # if entry_price is None:
-            #     entry_price = int(opp_entry_price + (sign * opp_entry_price * data['swingPercent'] / 100))
+            if opp_tp_order['ordStatus'] == 'Filled':
+                return
+            if entry_price is None:
+                entry_price = int(opp_entry_price + (sign * opp_entry_price * data['swingPercent'] / 100))
+            
             # if tp_order is None:
             #     tp_price = int(entry_price + (sign * entry_price * data['profitPercent'] / 100))
             # else:
             #     tp_price = tp_order['price']
-            #
             # losing_sum = opp_qty * abs((1/opp_entry_price) - (1/tp_price))
             # for n in range(1, 10000):
             #     gaining_sum = (opp_qty + n) * abs((1/entry_price) - (1/tp_price))
