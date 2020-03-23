8e9736e1905cf10a1aa39482a0e6c38d99c8e97f
Janakiraman Ganesan <vg2jana@gmail.com>
2020-03-23 16:25:01 +0530

    OANDA complete logic.

--- exchanges/OANDA/oanda_swing.py
+++ exchanges/OANDA/oanda_swing.py
@@ -28,7 +28,7 @@ def market_order(instrument, units, tp_price=None, tp_pips=None):
     if tp_price is not None:
         data["order"]["takeProfitOnFill"] = {"price": tp_price}
     elif tp_pips is not None:
-        data["order"]["takeProfitOnFill"] = {"distance": tp_pips}
+        data["order"]["takeProfitOnFill"] = {"pips": str(int(tp_pips))}
 
     r = orders.OrderCreate(accountID=account_id, data=data)
     client.request(r)
@@ -67,9 +67,9 @@ def market_if_touched_order(instrument, price, units, tp_price=None, tp_pips=Non
     }
 
     if tp_price is not None:
-        data["order"]["takeProfitOnFill"] = {"price": tp_price}
+        data["order"]["takeProfitOnFill"] = {"price": "{0:.5f}".format(tp_price)}
     elif tp_pips is not None:
-        data["order"]["takeProfitOnFill"] = {"distance": tp_pips}
+        data["order"]["takeProfitOnFill"] = {"pips": str(int(tp_pips))}
 
     r = orders.OrderCreate(accountID=account_id, data=data)
     client.request(r)
@@ -80,7 +80,7 @@ def market_if_touched_order(instrument, price, units, tp_price=None, tp_pips=Non
 def amend_trade(tradeID, tp_price=None, tp_pips=None):
     data = {"takeProfit": {}}
     if tp_price is not None:
-        data["takeProfit"] = {"price": tp_price}
+        data["takeProfit"] = {"price": "{0:.5f}".format(tp_price)}
     elif tp_pips is not None:
         data["takeProfit"] = {"distance": tp_pips}
 
@@ -110,7 +110,9 @@ def open_orders():
     client.request(r)
 
     for o in r.response['orders']:
-        ins = o['instrument']
+        ins = o.get('instrument', None)
+        if ins is None:
+            continue
         if ins not in result:
             result[ins] = []
         result[ins].append(o)
@@ -118,7 +120,7 @@ def open_orders():
     return result
 
 
-def get_positions():
+def open_positions():
     result = {}
     r = positions.OpenPositions(accountID=account_id)
     client.request(r)
@@ -129,14 +131,26 @@ def get_positions():
 
 
 def close_positions(instrument):
+    temp = o_positions.get(instrument, {})
+    if len(temp) == 0:
+        return
+    l_units = abs(int(temp["long"]["units"]))
+    s_units = abs(int(temp["short"]["units"]))
+
     data = {
-        "longUnits": "ALL",
-        "shortUnits": "ALL"
+        "longUnits": str(l_units) if l_units > 0 else "NONE",
+        "shortUnits": str(s_units) if s_units > 0 else "NONE"
     }
     r = positions.PositionClose(account_id, instrument, data)
-    client.request(r)
+    try:
+        client.request(r)
+    except oandapyV20.exceptions.V20Error as e:
+        log.warning(e)
+    else:
+        return r.response
 
-    return r.response
+
+
 
 
 class Symbol():
@@ -145,43 +159,57 @@ class Symbol():
         self.config = config
         self.first_order = None
         self.tp_pips = (10 ** (self.config['decimal'] - 1)) * self.config["takeProfit"]
+        self.l_units = 0
+        self.s_units = 0
+        self.last_side = None
 
     def clean(self):
         # Cancel pending orders
-        o_orders = open_orders()
-        cancel_orders([o['id'] for o in o_orders[self.instrument]])
+        cancel_orders([o['id'] for o in o_orders.get(self.instrument, [])])
         # Close positions
         close_positions(self.instrument)
+        # Clear first order reference
+        self.first_order = None
+        symbol.l_units = 0
+        symbol.s_units = 0
 
     def run(self):
+        o_pos = o_positions.get(self.instrument, {})
+        if len(o_pos) > 0:
+            symbol.l_units = abs(int(o_pos["long"]["units"]))
+            symbol.s_units = abs(int(o_pos["short"]["units"]))
         if self.first_order is None:
-            tp_pips = self.tp_pips / 2
-            self.first_order = market_order(self.instrument, self.config['startQty'], tp_pips=tp_pips)
+            self.first_order = market_order(self.instrument, self.config['startQty'])
+            m_price = float(self.first_order['orderFillTransaction']['price'])
+            tid = int(self.first_order['orderFillTransaction']['tradeOpened']['tradeID'])
+            amend_trade(tid, tp_price=m_price + self.config['takeProfit'] / 2)
             self.first_order["modified"] = False
+            self.last_side = 'buy'
         else:
             m_price = float(self.first_order['orderFillTransaction']['price'])
-            o_pos = o_positions[self.instrument]
-            o_ord = o_orders[self.instrument]
-            long_units = o_pos["long"]["units"]
-            short_units = o_pos["short"]["units"]
+            o_ord = o_orders.get(self.instrument, [])
+            l_units = symbol.l_units
+            s_units = symbol.s_units
 
             if len(o_ord) == 0:
-                if long_units < short_units:
-                    units = short_units * self.config['ratio'] - long_units
-                    price = m_price + self.config['swing']
-                else:
-                    units = long_units * self.config['ratio'] - short_units
-                    units *= -1
+                if l_units < s_units and self.last_side == 'sell':
+                    units = int(s_units * self.config['ratio'] - l_units)
+                    price = m_price
+                    tp_price = m_price + self.config['takeProfit']
+                    self.last_side = 'buy'
+                    market_if_touched_order(self.instrument, price, units, tp_price=tp_price)
+                elif s_units < l_units and self.last_side == 'buy':
+                    units = int(l_units * self.config['ratio'] - s_units) * -1
                     price = m_price - self.config['swing']
+                    tp_price = m_price - self.config['takeProfit']
+                    self.last_side = 'sell'
+                    market_if_touched_order(self.instrument, price, units, tp_price=tp_price)
 
-                market_if_touched_order(self.instrument, price, units, tp_pips=self.tp_pips)
-
-            if self.first_order["modified"] is False and short_units > 0:
+            if self.first_order["modified"] is False and s_units > 0:
                 tid = int(o_pos["long"]["tradeIDs"][0])
-                result = amend_trade(tid, tp_pips=self.tp_pips)
+                result = amend_trade(tid, tp_price=m_price + self.config['takeProfit'])
                 if result.get("errorMessage", None) is None:
                     self.first_order["modified"] = True
-
 
 
 logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO, filemode='a',
@@ -195,23 +223,34 @@ client = oandapyV20.API(access_token=token, environment="practice")
 account_id = params['global']['account_id']
 
 symbols = []
+o_positions = open_positions()
+o_orders = open_orders()
 for s, c in params["symbols"].items():
     symbol = Symbol(s, c)
     symbol.clean()
     symbols.append(symbol)
 
-refresh_time = 5
+time.sleep(2)
+refresh_time = 2
 stop_signal = False
 while True:
     if os.path.exists('STOP'):
         stop_signal = True
 
-    o_positions = get_positions()
+    o_positions = open_positions()
     o_orders = open_orders()
+
     for symbol in symbols:
         o_p = o_positions.get(symbol.instrument, {})
         if stop_signal is True and len(o_p) == 0:
             continue
+
+        if len(o_p) > 0:
+            long_units = abs(int(o_p["long"]["units"]))
+            short_units = abs(int(o_p["short"]["units"]))
+            if long_units < symbol.l_units or short_units < symbol.s_units:
+                symbol.clean()
+                continue
+
         symbol.run()
-        if len(o_p) > 0 and (o_p["long"]["units"] == 0 or o_p["short"]["units"] == 0):
-            symbol.clean()
+    time.sleep(2)
