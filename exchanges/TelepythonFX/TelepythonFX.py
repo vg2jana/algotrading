4a7d5766b7015108a39f70f7f67ffac44d5e6d14
Janakiraman Ganesan <jana.ganesan@tradingtechnologies.com>
2022-06-09 18:58:32 +0900

    Add Telegram dump option and place order based on dump signal.

--- exchanges/TelepythonFX/TelepythonFX.py
+++ exchanges/TelepythonFX/TelepythonFX.py
@@ -1,286 +1,137 @@
 import pandas as pd
 
-
 ########## MT5 imports ############
 
 import time
 import MetaTrader5 as mt5
 
-
-#### Operations parameters ##########
-
-lot = float(input('Input lot size: '))
-N_TP=float(input ('Numbers of TP? ')) #entre 1,2,3 (Number of tp that do you want to operate)
-SIN_SL=False
-sufix=input('Input sufix')
-
-
-
-
-####Telegram imports############
-
-import configparser
-import json
-import re
-import asyncio
-import pdb
-
-from telethon.errors import SessionPasswordNeededError
 from telethon import TelegramClient, events
-from telethon.tl.functions.messages import (GetHistoryRequest)
-from telethon.tl.types import (
-PeerChannel
-)
-
-#----------------------------------------------------------------------------------------------------------------------------------
-
-print("""\n /$$$$$$$$      /$$         /$$$$$$$            /$$    /$$                               /$$$$$$$$/$$   /$$        /$$$$$$  /$$$$$$ /$$$$$$$ /$$$$$$/$$$$$$$$/$$$$$$$ 
-|__  $$__/     | $$        | $$__  $$          | $$   | $$                              | $$_____| $$  / $$       /$$__  $$/$$__  $| $$__  $|_  $$_| $$_____| $$__  $$
-   | $$ /$$$$$$| $$ /$$$$$$| $$  \ $$/$$   /$$/$$$$$$ | $$$$$$$  /$$$$$$ /$$$$$$$       | $$     |  $$/ $$/      | $$  \__| $$  \ $| $$  \ $$ | $$ | $$     | $$  \ $$
-   | $$/$$__  $| $$/$$__  $| $$$$$$$| $$  | $|_  $$_/ | $$__  $$/$$__  $| $$__  $$      | $$$$$   \  $$$$/       | $$     | $$  | $| $$$$$$$/ | $$ | $$$$$  | $$$$$$$/
-   | $| $$$$$$$| $| $$$$$$$| $$____/| $$  | $$ | $$   | $$  \ $| $$  \ $| $$  \ $$      | $$__/    >$$  $$       | $$     | $$  | $| $$____/  | $$ | $$__/  | $$__  $$
-   | $| $$_____| $| $$_____| $$     | $$  | $$ | $$ /$| $$  | $| $$  | $| $$  | $$      | $$      /$$/\  $$      | $$    $| $$  | $| $$       | $$ | $$     | $$  \ $$
-   | $|  $$$$$$| $|  $$$$$$| $$     |  $$$$$$$ |  $$$$| $$  | $|  $$$$$$| $$  | $$      | $$     | $$  \ $$      |  $$$$$$|  $$$$$$| $$      /$$$$$| $$$$$$$| $$  | $$
-   |__/\_______|__/\_______|__/      \____  $$  \___/ |__/  |__/\______/|__/  |__/      |__/     |__/  |__/       \______/ \______/|__/     |______|________|__/  |__/
-                                     /$$  | $$                                                                                                                        
-                                    |  $$$$$$/                                                                                                                        
-                                     \______/                                                                                                                         """)
 
 
+def wait_for_signal():
+    global p
+    while True:
+        f.seek(p)
+        latest_data = f.read()
+        p = f.tell()
+        if latest_data:
+            yield latest_data.rstrip()
 
-
-
-print("""\n 
-
-.     .       .  .   . .   .   . .    +  .
-  .     .  :     .    .. :. .___---------___.
-       .  .   .    .  :.:. _".^ .^ ^.  '.. :"-_. .
-    .  :       .  .  .:../:            . .^  :.:\.
-        .   . :: +. :.:/: .   .    .        . . .:\
- .  :    .     . _ :::/:               .  ^ .  . .:\
-  .. . .   . - : :.:./.                        .  .:\
-  .      .     . :..|:                    .  .  ^. .:|
-    .       . : : ..||        .                . . !:|
-  .     . . . ::. ::\(                           . :)/
- .   .     : . : .:.|. ######              .#######::|
-  :.. .  :-  : .:  ::|.#######           ..########:|
- .  .  .  ..  .  .. :\ ########          :######## :/
-  .        .+ :: : -.:\ ########       . ########.:/
-    .  .+   . . . . :.:\. #######       #######..:/
-      :: . . . . ::.:..:.\           .   .   ..:/
-   .   .   .  .. :  -::::.\.       | |     . .:/
-      .  :  .  .  .-:.":.::.\             ..:/
- .      -.   . . . .: .:::.:.\.           .:/
-.   .   .  :      : ....::_:..:\   ___.  :/
-   .   .  .   .:. .. .  .: :.:.:\       :/
-     +   .   .   : . ::. :.:. .:.|\  .:/|
-     .         +   .  .  ...:: ..|  --.:|
-.      . . .   .  .  . ... :..:.."(  ..)"
- .   .       .      :  .   .: ::/  .  .::\
-
-                  by @MRAMIREZ
-
-""")
-
-#-------------------------------------------------------------------------------------------------------------------------------
- 
-
-#####################################################################
 
 # we show the data about the MetaTrader5 package
-
 print("MetaTrader5 package author: ", mt5.__author__)
 print("MetaTrader5 package version: ", mt5.__version__)
- 
+
 # We establish the connection with the MetaTrader 5 terminal
 
-if not mt5.initialize('C:\\Program Files\\HotForex Metatrader 5\\terminal64.exe'): #changue this for the path of your mt5
-    print("initialize() failed, error code =",mt5.last_error())
+if not mt5.initialize('C:\\Program Files\\XMTrading MT5\\terminal64.exe'):
+    print("initialize() failed, error code =", mt5.last_error())
     quit()
 
-
- 
 # We connect with the account indicating the password and the server (it is better to have it entered before in mt5)
+account_info = mt5.account_info()
+if account_info is not None:
+    account_info_dict = mt5.account_info()._asdict()
 
-account_info=mt5.account_info()
-if account_info!=None:
-     # we show the account data
-     #print(account_info)
-     # we display the data about the trading account in the form of a dictionary
-     #print("Show account_info()._asdict():") #I comment because is not necessary see it again
-     account_info_dict = mt5.account_info()._asdict()
-     #for prop in account_info_dict:
-         #print("  {}={}".format(prop, account_info_dict[prop]))
-     #print()
- 
-     # We transform the dictionary into a DataFrame and print it
-     df=pd.DataFrame(list(account_info_dict.items()),columns=['property','value'])
-     print("account_info() as dataframe:")
-     print(df)
-
-
-
-
+    # We transform the dictionary into a DataFrame and print it
+    df = pd.DataFrame(list(account_info_dict.items()), columns=['property', 'value'])
+    print("account_info() as dataframe:")
+    print(df)
 
 ###################################################################
 
+test_channel = -1001504705915 # Test Channel
+telepython_vip_channel = -1001302154811 # TelepythonFX VIP channel
+number_of_tp = 1
+lot = 0.01
 
-# We connect with our telegram account
-
-# Remember to use your own values from my.telegram.org!
-api_id = 14155598 #int value
-api_hash = '0a4c62b754438db563008a67f2197ec7'       #string value
-
-client = TelegramClient('anon', api_id, api_hash)
-
-#canal = -1001504705915 # Test channel
-canal= -1001302154811 #example  # to connect with vip signal channel (is the channel ID)
-
-
-print('\n Waiting for a signal')
-
-@client.on(events.NewMessage(chats=canal))
-
-
-################################################################################
-
-# This function is the one that listens for new messages
-
-async def newMessageListener(event):
-    # Get message text
-    print('------------------------------------------------------------NUEVA ORDEN--------------------------------------------------------------')
-    newMessage = event.message.message
-    print(newMessage)
-
-    lista_operacion=newMessage.split()
-    #pdb.set_trace()
+filename = "C:\\Users\\Administrator\\Documents\\TelepythonFX\\telegram_dump.txt"
+f = open(filename)
+p = 0
+f.seek(p)
+f.read()
+p = f.tell()
+for line in wait_for_signal():
+    print('\n Waiting for a signal')
     try:
-        if 'SELL' in lista_operacion[1]:
-            lista_operacion[1] = 'SELL'
-        elif 'BUY' in lista_operacion[1]:
-            lista_operacion[1] = 'BUY'
-        else:
-            return print("The message is not a signal \nWaiting for a signal")
-    except:
-        return print("The message is not a signal \nWaiting for a signal")
-    
-    if 'GOLD' in lista_operacion[0]: #Some Brokers use GOLD instead of XAUUSD, if not your case, comment this line
-        lista_operacion[0]='XAUUSD'
-    else:
-        lista_operacion[0]=lista_operacion[0][1:]
+        signal = dict(eval(line))
+    except Exception as e:
+        print("Error when parsing signal line from file:")
+        print(e)
+        continue
 
-    ########################################
-    par=lista_operacion[0]+sufix
-    op=lista_operacion[1]
-    #entrada=float(lista_operacion[2])
-    SL=float(lista_operacion[10])
-    TP1=float(lista_operacion[4])
-    try:
-        TP2=float(lista_operacion[6]) #some signals dont provide tp2
-    except:
-        TP2='--'
-    try:
-        TP3=float(lista_operacion[8]) #same but for tp3
-    except:
-        TP3='--'
+    if signal["ChannelId"] != telepython_vip_channel:
+        print("Message is for a different channel: {}".format(signal["ChannelId"]))
+        continue
 
-    print(newMessage.split())
-    ########################################
-    
-    #Now we are going to pull the trade to MT5
-    # we prepare the structure of the purchase requestpreparamos la estructura de la solicitud de compra
-    symbol = par
+    symbol = signal["Symbol"]
+    if symbol == "XAUUSD":
+        symbol = "GOLD"
+
+    side = signal["Side"]
+    stop_loss = signal["StopLoss"]
+
     symbol_info = mt5.symbol_info(symbol)
-    #print(symbol_info)
     if symbol_info is None:
-            print(symbol, "not found, can not call order_check()")
-            mt5.shutdown()
-            quit()
- 
+        print(symbol, "not found, can not call order_check()")
+        mt5.shutdown()
+        quit()
+
     # if the symbol is not available in MarketWatch, we add it
     if not symbol_info.visible:
-            print(symbol, "is not visible, trying to switch on")
-            if not mt5.symbol_select(symbol,True):
-                print("symbol_select({}}) failed, exit",symbol)
-                mt5.shutdown()
-                quit()
+        print(symbol, "is not visible, trying to switch on")
+        if not mt5.symbol_select(symbol, True):
+            print("symbol_select({}}) failed, exit", symbol)
+            mt5.shutdown()
+            quit()
 
-    
     point = mt5.symbol_info(symbol).point
-    
+
     deviation = 20
-    if op=='BUY':
-        op_type=mt5.ORDER_TYPE_BUY
-        price= mt5.symbol_info_tick(symbol).ask
-        opposite_op_type=mt5.ORDER_TYPE_SELL
-    if op=='SELL':
-        op_type=mt5.ORDER_TYPE_SELL
-        price= mt5.symbol_info_tick(symbol).bid
-        opposite_op_type=mt5.ORDER_TYPE_BUY
+    if side == 'BUY':
+        op_type = mt5.ORDER_TYPE_BUY
+        price = mt5.symbol_info_tick(symbol).ask
+    else:
+        op_type = mt5.ORDER_TYPE_SELL
+        price = mt5.symbol_info_tick(symbol).bid
 
-    TP_list=[TP1,TP2,TP3]
+    # Check for existing orders for the symbol with same side
+    symbol_orders = mt5.positions_get(symbol=symbol)
+    if symbol_orders is not None:
+        if any(p.type == op_type for p in symbol_orders):
+            print("Order already exists for symbol {} with same side".format(symbol))
+            continue
 
-    #print(mt5.ORDER_FILLING_RETURN)
-
-    TP_cont=1
-    for TP in TP_list:
-        if TP_cont>N_TP:
-            break
-        if TP_cont==1: # we put SL only to the first TP (if you want to put them all, just add them here TP_cont ==2 and TP_cont==3)
-            request = {
-                    "action": mt5.TRADE_ACTION_DEAL,
-                    "symbol": symbol,
-                    "volume": lot,
-                    "type": op_type,
-                    "price": price,
-                    "sl": SL,
-                    "tp": TP,
-                    "deviation": deviation,
-                    "magic": 234000,
-                    "comment": "TelePython FX copier",
-                    "type_time": mt5.ORDER_TIME_GTC,
-                    "type_filling": mt5.ORDER_FILLING_FOK,
-                }
-        if TP_cont!=1: #change this if you want to put SL to the different position 
-            request = {
-                    "action": mt5.TRADE_ACTION_DEAL,
-                    "symbol": symbol,
-                    "volume": lot,
-                    "type": op_type,
-                    "price": price,
-                    "tp": TP,
-                    "deviation": deviation,
-                    "magic": 234000,
-                    "comment": "TelePython FX copier",
-                    "type_time": mt5.ORDER_TIME_GTC,
-                    "type_filling": mt5.ORDER_FILLING_FOK,
-                }
+    tpList = signal["TakeProfits"][:number_of_tp]
+    for take_profit in tpList:
+        request = {
+            "action": mt5.TRADE_ACTION_DEAL,
+            "symbol": symbol,
+            "volume": 0.01,
+            "type": op_type,
+            "price": price,
+            "sl": stop_loss,
+            "tp": take_profit,
+            "deviation": deviation,
+            "magic": 234000,
+            "comment": "TelePython FX copier",
+            "type_time": mt5.ORDER_TIME_GTC,
+            "type_filling": mt5.ORDER_FILLING_IOC,
+        }
 
         # we send the commercial request
-
         result = mt5.order_send(request)
         # we check the result of the execution
-        print("R. order_send(): by {} {} lots at {} with deviation={} points and TP = {} ".format(symbol,lot,price,deviation,TP) );
+        print("R. order_send(): by {} {} lots at {} with deviation={} points and TP = {} ".format(symbol, lot, price,
+                                                                                                  deviation, take_profit))
         if result.retcode != mt5.TRADE_RETCODE_DONE:
-                print("2. order_send failed, retcode={}".format(result.retcode))
-             # we request the result in the form of a dictionary and display it element by element
-                result_dict=result._asdict()
-                for field in result_dict.keys():
-                    print("   {}={}".format(field,result_dict[field]))
-                    # if it is the structure of a commercial request, we also show it element by element
-                    if field=="request":
-                            traderequest_dict=result_dict[field]._asdict()
-                            for tradereq_filed in traderequest_dict:
-                                print("       traderequest: {}={}".format(tradereq_filed,traderequest_dict[tradereq_filed]))
-                #print("shutdown() and quit")
-                #mt5.shutdown()
-                #quit()
- 
-        #print("2. order_send done, ", result)
-        
-        TP_cont=TP_cont+1
-
-    print('\n Waiting for a signal')
-
-with client:
-    client.run_until_disconnected()
+            print("2. order_send failed, retcode={}".format(result.retcode))
+            # we request the result in the form of a dictionary and display it element by element
+            result_dict = result._asdict()
+            for field in result_dict.keys():
+                print("   {}={}".format(field, result_dict[field]))
+                # if it is the structure of a commercial request, we also show it element by element
+                if field == "request":
+                    traderequest_dict = result_dict[field]._asdict()
+                    for tradereq_filed in traderequest_dict:
+                        print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
--- exchanges/TelepythonFX/dump_telegram_messages.py
+++ exchanges/TelepythonFX/dump_telegram_messages.py
@@ -22,13 +22,110 @@ api_hash = '0a4c62b754438db563008a67f2197ec7'       #string value
 client = TelegramClient('anon', api_id, api_hash)
 
 test_channel = -1001504705915 # Test Channel
-vip_channel = -1001302154811 # TelepythonFX VIP channel
+telepython_vip_channel = -1001302154811 # TelepythonFX VIP channel
 chatList = [test_channel,]# vip_channel]
 
 outFile = "C:\\Users\\Administrator\\Documents\\TelepythonFX\\telegram_dump.txt"
-
+# outFile = "telegram_dump.txt"
 
 print('\n Waiting for a signal')
+
+
+class Signal:
+    def __init__(self):
+        self.chat_id = None
+        self.symbol = None
+        self.side = None
+        self.stop_loss = None
+        self.take_profits = []
+        self.error = False
+
+    def message(self):
+        dict_message = {
+            "ChannelId": self.chat_id,
+            "Symbol": self.symbol,
+            "Side": self.side,
+            "StopLoss": self.stop_loss,
+            "TakeProfits": self.take_profits
+        }
+
+        return dict_message
+
+
+def telepythonFX_VIP(messageList):
+    signal = Signal()
+    try:
+        if 'SELL' in messageList[1]:
+            messageList[1] = 'SELL'
+        elif 'BUY' in messageList[1]:
+            messageList[1] = 'BUY'
+        else:
+            signal.error = True
+            return print("The message is not a signal \nWaiting for a signal")
+    except:
+        signal.error = True
+        return print("The message is not a signal \nWaiting for a signal")
+
+    if 'GOLD' in messageList[0]:
+        messageList[0] = 'XAUUSD'
+    else:
+        messageList[0] = messageList[0][1:]
+
+    signal.symbol = messageList[0]
+    signal.side = messageList[1]
+    signal.stop_loss = messageList[10]
+    signal.take_profits.append(messageList[4])
+    try:
+        signal.take_profits.append((messageList[6]))
+    except:
+        pass
+    try:
+        signal.take_profits.append((messageList[8]))
+    except:
+        pass
+
+    return signal
+
+
+def blackswan_fx(messageList):
+    signal = Signal()
+    messageList = [m.replace(" ", "") for m in messageList]
+    try:
+        if messageList[1] != 'INSTANT':
+            signal.error = True
+            return print("The message is not a signal \nWaiting for a signal")
+        if 'SELL' in messageList[2]:
+            messageList[1] = 'SELL'
+        elif 'BUY' in messageList[2]:
+            messageList[1] = 'BUY'
+        else:
+            signal.error = True
+            return print("The message is not a signal \nWaiting for a signal")
+    except:
+        signal.error = True
+        return print("The message is not a signal \nWaiting for a signal")
+
+    messageList[0] = messageList[0].replace("/", "")
+    if 'GOLD' in messageList[0]:
+        messageList[0] = 'XAUUSD'
+    else:
+        messageList[0] = messageList[0][1:]
+
+    signal.symbol = messageList[0]
+    signal.side = messageList[2]
+
+    try:
+        for index, m in enumerate(messageList):
+            if m == "TP:" and messageList[index+1] != 'OPEN':
+                signal.take_profits.append(messageList[index+1])
+            elif m == "SL:":
+                signal.stop_loss = messageList[index+1]
+    except Exception as e:
+        signal.error = True
+        return print(e)
+
+    return signal
+
 
 @client.on(events.NewMessage(chats=chatList))
 async def newMessageListener(event):
@@ -36,51 +133,22 @@ async def newMessageListener(event):
     newMessage = event.message.message
     print(newMessage)
 
-    messageList = newMessage.split()
-    #pdb.set_trace()
-    try:
-        if 'SELL' in messageList[1]:
-            messageList[1] = 'SELL'
-        elif 'BUY' in messageList[1]:
-            messageList[1] = 'BUY'
-        else:
-            return print("The message is not a signal \nWaiting for a signal")
-    except:
-        return print("The message is not a signal \nWaiting for a signal")
-    
-    if 'GOLD' in messageList[0]: #Some Brokers use GOLD instead of XAUUSD, if not your case, comment this line
-        messageList[0] = 'XAUUSD'
-    else:
-        messageList[0] = messageList[0][1:]
+    telegram_message = newMessage.split()
 
-    ########################################
-    symbol = messageList[0]
-    side = messageList[1]
-    stopLoss = float(messageList[10])
-    TP1 = float(messageList[4])
-    TP2 = 0
-    TP3 = 0
-    try:
-        TP2 = float(messageList[6]) #some signals dont provide tp2
-    except:
-        pass
-    try:
-        TP3 = float(messageList[8]) #same but for tp3
-    except:
-        pass
+    retval = None
+    if chatId == telepython_vip_channel:
+        retval = telepythonFX_VIP(telegram_message)
+    elif chatId == test_channel:
+        retval = blackswan_fx(telegram_message)
 
-    dictMessage = {
-            "ChannelId": chatId,
-            "Symbol": symbol,
-            "Side": side,
-            "StopLoss": stopLoss,
-            "TP1": TP1,
-            "TP2": TP2,
-            "TP3": TP3
-        }
+    if retval is None or retval.error is True:
+        return
 
+    retval.chat_id = chatId
+    dump_message = retval.message()
+    print("Dumping to file: {}".format(dump_message))
     with open(outFile, 'a') as f:
-        f.write("{}\n".format(dictMessage))
+        f.write("{}\n".format(dump_message))
 
     print('\n Waiting for a signal')
 
